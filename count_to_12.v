4a6dee27d74b1ca397f6e78c73f28c1c582a7ec0
Geoffrey Betts <geoffreybetts@hotmail.com>
2022-09-16 10:48:43 +0100

    hotfix and comments

--- count_to_12.v
+++ count_to_12.v
@@ -27,30 +27,30 @@ module count_to_12(
     output [7:0] q	// counter output
     );
 	 wire carry;
-	 reg ro, ro_del;
+	 reg ro;
 	 
-	 decade_counter first_digit	(.clk(clk),					// First digit
-											 .reset(reset|ro_del),		// Normal decade counter
+	 decade_counter first_digit	(.clk(clk),						// First digit
+											 .reset(reset|out),			// Normal decade counter
 											 .ena(ena|reset),
 											 .out(carry),
 											 .q(q[3:0]));
-																				// For second digit, 'out' isn't used
-	 decade_counter second_digit	(.clk(clk),	// Tied to rollover of first digit (reset is also tied due to being synchronous)
-											 .reset(reset|ro_del), 			// Either reset-high or during rollover
-											 .ena((ena&carry)|(ena&ro_del)|reset),
-											 .q(q[7:4]));					
+																								// For second digit, 'out' isn't used
+	 decade_counter second_digit	(.clk(clk),										// Tied to rollover of first digit (reset is also tied due to being synchronous)
+											 .reset(reset|out), 							// Either reset-high or during rollover
+											 .ena((ena&carry)|(ena&out)|reset), 	// Also enabled when ro_del is high. Different to 0-59 counter as carry is high when first_digit==4'h9, but
+											 .out(),											// Unconnected as not req. (Still gives a warning at synthesis tho)
+											 .q(q[7:4]));									// here has to roll over when the first digit==4'h1
 	
 
-	 always @(posedge clk) begin	// Always block to control 'out'.
-		if (reset) begin
-			ro <= 1'b0;
-			ro_del <= 1'b0;
-			out <= 1'b0;
-		end else if (ena) begin
-			ro <= (q==8'h9);	// Resets counter at 11.
-			ro_del <= ro;		// high during reset to allow pm_toggle to reset to 1'b0
-			out <= ro;			// Otherwise follows ro, ends up trailing it by 1 clock cycle (effectively)
-			
+	 always @(posedge clk) begin	
+		if (ena) begin
+			if (reset) begin		// Change everything to low at reset
+				ro <= 1'b0;
+				out <= 1'b0;
+			end else begin
+				ro <= (q==8'h9);	// ro high during q==8'h10
+				out <= ro;			// out is ro delayed by one ena signal thus high when q==8'h11
+			end
 		end
 	 end
 	 

c3b98a9068dce06bf863230e3ab677d2fafd8d28
Geoffrey Betts <geoffreybetts@hotmail.com>
2022-09-22 11:37:50 +0100

    Implemented input device
    More testing needed however

--- count_to_60_updown.v
+++ count_to_60_updown.v
@@ -24,35 +24,40 @@ module count_to_60_updown(
     input i_ena,
     input i_up,
     input i_down,
+	 input i_wr,
+	 input [7:0] i_in,
     output [7:0] o_q
     );
 	 
 	 reg r_ru, r_rd, r_carry_up_del, r_carry_down_del;
+	 wire [3:0] w_in;
 	 
 	 decade_counter_updown first_digit 	(.i_clk(i_clk),
 													 .i_reset(i_reset),
-													 .i_ena((i_ena&w_in_c)|i_reset),
+													 .i_ena((i_ena&(w_in_c|i_wr))|i_reset),
 													 .i_up(i_up),
 													 .i_down(i_down),
-													 .i_wr(1'b0),
-													 .i_in(4'h0),
+													 .i_wr(i_wr),
+													 .i_in(i_in[3:0]),
 													 .o_out_up(w_carry_up),
 													 .o_out_down(w_carry_down),
 													 .o_q(o_q[3:0]));
 													
 	 decade_counter_updown second_digit (.i_clk(i_clk),
 													 .i_reset(i_reset|r_ru),
-													 .i_ena((i_ena&w_carry_del_c&w_in_c)|i_reset),
+													 .i_ena((i_ena&w_carry_del_c&w_in_c)|(i_ena&i_wr)|i_reset),
 													 .i_up((w_carry_down&r_carry_up_del)&i_up),
 													 .i_down(w_carry_up&r_carry_down_del&i_down&~i_up),
-													 .i_wr(r_rd&~i_up),
-													 .i_in(4'h5),
+													 .i_wr((r_rd&~i_up)|i_wr),
+													 .i_in(w_in),
 													 .o_out_up(),
 													 .o_out_down(),
 													 .o_q(o_q[7:4]));
 	
 	 assign w_in_c = i_up|i_down;
 	 assign w_carry_del_c = r_carry_up_del|r_carry_down_del;
+	 assign w_in = i_wr? i_in[7:4]:4'h5;
+	 
 	 always @(posedge i_clk) begin
 		if (i_ena) begin
 			r_rd <= 1'b0;
--- input_device.v
+++ input_device.v
@@ -27,46 +27,69 @@ module input_device(
     input i_val_dec_btn,
     input i_sel_inc_btn,
     input i_sel_dec_btn,
+	 input [7:0] i_in,
 	 output o_reset,
 	 output o_wr,
     output [1:0] o_sel,
     output [7:0] o_val
     );
 	 
+	 wire [1:0] unused;
+	 
 	 button_debounce reset_btn   (.i_clk(i_clk),				//Hit reset button, sends reset signal to device
 											.i_ena(i_ena),
 											.i_btn(i_reset_btn),
-											.i_pulse(o_reset));
+											.o_pulse(o_reset));
 										 
 	 button_debounce wr_btn	 	  (.i_clk(i_clk),
 											.i_ena(i_ena),
 											.i_btn(i_wr_btn),
-											.i_pulse(w_wr_pulse));										 
+											.o_pulse(w_wr_pulse));										 
 							
 	 button_debounce val_inc_btn (.i_clk(i_clk),
 										   .i_ena(i_ena),
 											.i_btn(i_val_inc_btn),
-											.i_pulse(w_val_inc_pulse));							
+											.o_pulse(w_val_inc_pulse));							
 
 	 button_debounce val_dec_btn (.i_clk(i_clk),
 											.i_ena(i_ena),
 											.i_btn(i_val_dec_btn),
-											.i_pulse(w_val_dec_pulse));
+											.o_pulse(w_val_dec_pulse));
 										 
 	 button_debounce sel_inc_btn (.i_clk(i_clk),
 											.i_ena(i_ena),
 											.i_btn(i_sel_inc_btn),
-											.i_pulse(w_sel_inc_pulse));
+											.o_pulse(w_sel_inc_pulse));
 
 	 button_debounce sel_dec_btn (.i_clk(i_clk),
 											.i_ena(i_ena),
 											.i_btn(i_sel_dec_btn),
-											.i_pulse(w_sel_dec_pulse));
+											.o_pulse(w_sel_dec_pulse));
 											
 											
 	 my_dff wr_toggle (.i_clk(i_clk),
-								 .i_ena(w_wr_pulse),
+								 .i_ena(w_wr_pulse|o_reset),
 								 .i_d((~o_wr)|o_reset),
 								 .o_q(o_wr));
+								 
+	 count_to_60_updown val_change (.i_clk(i_clk),
+											  .i_reset(o_reset),
+											  .i_ena(i_ena),
+											  .i_up(w_val_inc_pulse),
+											  .i_down(w_val_dec_pulse),
+											  .i_wr((w_sel_inc_pulse|w_sel_dec_pulse)),
+											  .i_in(i_in),
+											  .o_q(o_val));
+											  
+	 decade_counter_updown sel_change (.i_clk(i_clk),
+												  .i_reset(o_reset),
+												  .i_ena(i_ena&(w_sel_inc_pulse|w_sel_dec_pulse)|o_reset),
+												  .i_up(w_sel_inc_pulse),
+												  .i_down(w_sel_dec_pulse),
+												  .i_wr(1'b0),
+												  .i_in(4'h0),
+												  .o_out_up(),
+												  .o_out_down(),
+												  .o_q({unused[1:0], o_sel[1:0]}));
 										 
 endmodule
